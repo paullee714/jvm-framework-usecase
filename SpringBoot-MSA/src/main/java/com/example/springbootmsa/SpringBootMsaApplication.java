13092cab07f2811f9985feb94aeaaf8ed74aeed9
paullee714 <paul_lee@kakao.com>
2022-04-23 13:11:16 +0900

    feat: SpringBoot-MSA 스터디 소스 init

---
+++ SpringBoot-MSA/src/main/java/com/example/springbootmsa/SpringBootMsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMsaApplication.class, args);
+    }
+
+}
---
+++ SpringBoot-MSA/src/test/java/com/example/springbootmsa/SpringBootMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
