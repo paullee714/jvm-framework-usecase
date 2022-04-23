76e4609e0b532c6bb4390cc4fbf14c625d5e4628
paullee714 <paul_lee@kakao.com>
2022-04-23 13:48:48 +0900

    feat: SpringBoot-MSA 계산기 예제 프로그램 저장소 세팅

--- SpringBoot-MSA-Calculator/src/main/java/com/example/springbootmsacalculator/SpringBootMsaCalculatorApplication.java
+++ SpringBoot-MSA-Calculator/src/main/java/com/example/springbootmsacalculator/SpringBootMsaCalculatorApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootmsa;
+package com.example.springbootmsacalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMsaApplication {
+public class SpringBootMsaCalculatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMsaApplication.class, args);
+        SpringApplication.run(SpringBootMsaCalculatorApplication.class, args);
     }
 
 }
