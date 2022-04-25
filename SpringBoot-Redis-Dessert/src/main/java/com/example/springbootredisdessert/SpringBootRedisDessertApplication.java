4e4c28c7c573918a0c7ebd8c4e80c705b2d9f3a0
paullee714 <paul_lee@kakao.com>
2022-04-26 01:53:40 +0900

    feat: SpringBoot-Redis-Dessert 예제 소스 초기세팅
    - Redis docker-compose 파일 작성
    - README 초기화
    - Gradle 세팅

---
+++ SpringBoot-Redis-Dessert/src/main/java/com/example/springbootredisdessert/SpringBootRedisDessertApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootredisdessert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisDessertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisDessertApplication.class, args);
+    }
+
+}
