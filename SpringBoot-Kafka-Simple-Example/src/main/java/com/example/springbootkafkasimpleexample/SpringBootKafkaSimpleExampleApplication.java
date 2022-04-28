e0dd4ad356f5807b5cc9ec8e2eebce0df9389ca8
paullee714 <paul_lee@kakao.com>
2022-04-28 19:36:50 +0900

    feat: Spring kafka 예제 초기세팅

---
+++ SpringBoot-Kafka-Simple-Example/src/main/java/com/example/springbootkafkasimpleexample/SpringBootKafkaSimpleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkasimpleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaSimpleExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaSimpleExampleApplication.class, args);
+    }
+
+}
