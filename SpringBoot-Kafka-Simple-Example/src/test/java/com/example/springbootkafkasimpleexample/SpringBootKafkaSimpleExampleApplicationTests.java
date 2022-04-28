338565ae5e28a85e3a091641b0cca72ceba37135
paullee714 <paul_lee@kakao.com>
2022-04-28 19:36:50 +0900

    feat: Spring kafka 예제 초기세팅

---
+++ SpringBoot-Kafka-Simple-Example/src/test/java/com/example/springbootkafkasimpleexample/SpringBootKafkaSimpleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkasimpleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaSimpleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
