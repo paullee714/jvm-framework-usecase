f659b902f19bc8d7f5bc393ef98dcac70b5477e4
paullee714 <paul_lee@kakao.com>
2022-04-30 14:58:17 +0900

    feat: consumer SpringBoot Application 작성

---
+++ SpringBoot-Kafka-Simple-Example/Consumer-Application/src/test/java/com/example/consumerapplication/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SpringBoot-Kafka-Simple-Example/Producer-Application/src/main/java/com/example/producerapplication/controller/ProduceController.java
@@ -0,0 +1,2 @@
+package com.example.producerapplication.controller;public class ProduceController {
+}
