f135cc34a8d85131128364ab8b91ce6f5e14d7d1
paullee714 <paul_lee@kakao.com>
2022-05-01 16:19:17 +0900

    feat: SpringBoot Intercept를 활용한 Request 정보 로깅하기

---
+++ SpringBoot-Handler-Interceptor-Simple/src/main/java/com/example/springboothandlerinterceptorsimple/SpringBootHandlerInterceptorSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothandlerinterceptorsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHandlerInterceptorSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHandlerInterceptorSimpleApplication.class, args);
+    }
+
+}
