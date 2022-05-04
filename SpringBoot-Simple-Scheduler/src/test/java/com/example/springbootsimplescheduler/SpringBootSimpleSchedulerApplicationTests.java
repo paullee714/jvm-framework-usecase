7e4738e15fdce34b2b488f3977a83cb4af2fb918
paullee714 <paul_lee@kakao.com>
2022-05-04 10:34:56 +0900

    feat: SpringBoot 스케쥴러 프로젝트 생성

---
+++ SpringBoot-Simple-Scheduler/src/test/java/com/example/springbootsimplescheduler/SpringBootSimpleSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsimplescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
