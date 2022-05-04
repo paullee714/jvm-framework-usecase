f3358222405961d32b467684ae2bf552ab1d6977
paullee714 <paul_lee@kakao.com>
2022-05-04 10:34:56 +0900

    feat: SpringBoot 스케쥴러 프로젝트 생성

---
+++ SpringBoot-Simple-Scheduler/src/main/java/com/example/springbootsimplescheduler/SpringBootSimpleSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsimplescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSimpleSchedulerApplication.class, args);
+    }
+
+}
