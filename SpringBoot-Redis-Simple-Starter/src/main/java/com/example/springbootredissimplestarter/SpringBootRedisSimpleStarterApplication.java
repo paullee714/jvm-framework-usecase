a3c416d4e38fe5d0131e58beda59aac989b5df70
paullee714 <paul_lee@kakao.com>
2022-05-08 14:57:09 +0900

    fix: fixed untracked files

---
+++ SpringBoot-Redis-Simple-Starter/src/main/java/com/example/springbootredissimplestarter/SpringBootRedisSimpleStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootredissimplestarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisSimpleStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisSimpleStarterApplication.class, args);
+    }
+
+}
