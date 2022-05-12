6ba32c27d14e5d878af7ab5e9f7b4cfe1febd4cf
paullee714 <paul_lee@kakao.com>
2022-05-12 17:04:01 +0900

    feat: MSA-NBATeam 모듈 생성 및 Eureka 등록

---
+++ SpringCloud-Auth-MSA/MSA-NBATeam/src/main/java/com/example/msanbateam/controller/TeamController.java
@@ -0,0 +1,22 @@
+package com.example.msanbateam.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @GetMapping("/server-info")
+    public String getPlayer() {
+        return "SERVER: [" + name + "] Team Server is running on port " + port;
+    }
+}
---
+++ SpringCloud-Auth-MSA/MSA-NBATeam/src/test/java/com/example/msanbateam/MsaNbaTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msanbateam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaNbaTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
