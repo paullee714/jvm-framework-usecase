c3aaefebbaca7fb08c441d2b0448889081d262dc
paullee714 <paul_lee@kakao.com>
2022-05-16 16:18:59 +0900

    feat: SpringCloud Gateway predicates 및 filter 테스트

--- SpringCloud-Auth-MSA/MSA-NBATeam/src/main/java/com/example/msanbateam/controller/TeamController.java
+++ SpringCloud-Auth-MSA/MSA-NBATeam/src/main/java/com/example/msanbateam/controller/TeamController.java
@@ -2,6 +2,7 @@ package com.example.msanbateam.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,12 @@ public class TeamController {
 
     @GetMapping("/server-info")
     public String getPlayer() {
+        return "SERVER: [" + name + "] Team Server is running on port " + port;
+    }
+
+    @PostMapping("/server-info")
+    public String postPlayer() {
+
         return "SERVER: [" + name + "] Team Server is running on port " + port;
     }
 }
