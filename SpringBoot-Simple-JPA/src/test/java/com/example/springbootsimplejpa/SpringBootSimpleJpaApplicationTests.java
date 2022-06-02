4e2f884fc90ffafbd5f325e9dcba1b1a44a1039c
paullee714 <paul_lee@kakao.com>
2022-06-02 17:54:11 +0900

    feat: init SpringBoot JPA repo

---
+++ SpringBoot-Simple-JPA/src/test/java/com/example/springbootsimplejpa/SpringBootSimpleJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsimplejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
