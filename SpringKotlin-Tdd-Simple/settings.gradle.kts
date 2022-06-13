b8aa3809a7f3a241fd27910cb32c2a62170ea31e
paullee714 <paul_lee@kakao.com>
2022-06-13 14:46:26 +0900

    feat: SpringKotlin Project Init

---
+++ SpringKotlin-Tdd-Simple/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "SpringKotlin-Tdd-Simple"
---
+++ SpringKotlin-Tdd-Simple/src/main/kotlin/com/example/springkotlintddsimple/SpringKotlinTddSimpleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkotlintddsimple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinTddSimpleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinTddSimpleApplication>(*args)
+}
