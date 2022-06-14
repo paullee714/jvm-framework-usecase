7d909019cbb3a671646555effa93284ec011e388
paullee714 <paul_lee@kakao.com>
2022-06-14 16:42:20 +0900

    feat: Service 패키지 Kotlin객체 주입

--- SpringKotlin-Tdd-Simple/src/main/kotlin/com/example/springkotlintddsimple/service/AssociationService.kt
+++ SpringKotlin-Tdd-Simple/src/main/kotlin/com/example/springkotlintddsimple/service/AssociationService.kt
@@ -5,13 +5,13 @@ import com.example.springkotlintddsimple.domain.AssociationName
 import com.example.springkotlintddsimple.handler.AssociationErrorResult
 import com.example.springkotlintddsimple.handler.exception.AssociationException
 import com.example.springkotlintddsimple.repository.AssociationRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 
 @Service
-class AssociationService {
+class AssociationService(private val associationRepository: AssociationRepository) {
 
-    lateinit var associationRepository: AssociationRepository
 
     fun registAssociation(userUuid: String, associationName: AssociationName, point: Int): Association? {
 
