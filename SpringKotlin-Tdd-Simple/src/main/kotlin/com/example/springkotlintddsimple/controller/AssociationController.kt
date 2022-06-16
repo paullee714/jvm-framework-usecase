88e08f3c89a8ed6558c2bfda9b2a9cf6d22ad622
paullee714 <paul_lee@kakao.com>
2022-06-16 13:44:52 +0900

    feat: Controller Endpoint 및 Request DTO 추가

--- SpringKotlin-Tdd-Simple/src/main/kotlin/com/example/springkotlintddsimple/controller/AssociationController.kt
+++ SpringKotlin-Tdd-Simple/src/main/kotlin/com/example/springkotlintddsimple/controller/AssociationController.kt
@@ -1,5 +1,22 @@
 package com.example.springkotlintddsimple.controller
 
+import com.example.springkotlintddsimple.domain.dto.AssociationRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 class AssociationController {
+
+    @PostMapping("/api/v1/association")
+    fun createAssociation(
+        @RequestHeader("USER_UUID") user_UUID: String,
+        @RequestBody associationRequest: AssociationRequest,
+    ): ResponseEntity<AssociationRequest> {
+
+        return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
 }
