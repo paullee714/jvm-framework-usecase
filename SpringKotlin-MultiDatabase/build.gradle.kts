3749478b186b9e24e5e916333e3af94ad8c1b442
paullee714 <paul_lee@kakao.com>
2022-08-25 18:27:56 +0900

    fix: Unused Method, Package 삭제

--- SpringKotlin-MultiDatabase/build.gradle.kts
+++ SpringKotlin-MultiDatabase/build.gradle.kts
@@ -24,7 +24,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -33,7 +32,6 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     runtimeOnly("org.postgresql:postgresql")
-
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- SpringKotlin-MultiDatabase/src/main/kotlin/wool/multidb/config/MongoConfig.kt
+++ SpringKotlin-MultiDatabase/src/main/kotlin/wool/multidb/config/MongoConfig.kt
@@ -2,14 +2,11 @@ package wool.multidb.config
 
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
-import com.mongodb.MongoCredential
-import com.mongodb.ServerAddress
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
-import java.util.Collections
 
 
 @Configuration
