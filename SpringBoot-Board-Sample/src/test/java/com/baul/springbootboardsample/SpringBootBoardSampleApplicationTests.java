a6570e5449b01f1e4494e2315bb3b5ea5781e947
paullee714 <paul_lee@kakao.com>
2022-09-26 07:53:28 +0900

    feat: board Project Initialize

---
+++ SpringBoot-Board-Sample/src/test/java/com/baul/springbootboardsample/SpringBootBoardSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baul.springbootboardsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
